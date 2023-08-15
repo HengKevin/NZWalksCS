9263c9ca1273ce6f8017f09cfd6ff587c3376746
Khefff <heng.kevin19@kit.edu.kh>
2023-08-15 09:54:08 +0700

    migration for images table

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -14,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "NZ Walk API", Version = "v1"});
